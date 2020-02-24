83ce604e3e3cf5d79466102d58689eb99779f722
Mohamed Jerbi <mohamed.jerbi2012@gmail.com>
2020-02-24 15:16:42 +0100

    Updates

---
+++ precache-manifest.fa65a36ea3ebb8578291889aac098e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0cf52d9ab52add615360067469817a",
+    "url": "/Opportunities/index.html"
+  },
+  {
+    "revision": "071ba57edaab9fdf4793",
+    "url": "/Opportunities/static/css/main.b96dfcc4.chunk.css"
+  },
+  {
+    "revision": "3d917f6df4aaedef95d1",
+    "url": "/Opportunities/static/js/2.f1d7c6c6.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Opportunities/static/js/2.f1d7c6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071ba57edaab9fdf4793",
+    "url": "/Opportunities/static/js/main.18f9094b.chunk.js"
+  },
+  {
+    "revision": "d29bb5ee776e609567ef",
+    "url": "/Opportunities/static/js/runtime-main.5cb7aecf.js"
+  }
+]);
